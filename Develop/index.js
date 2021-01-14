524d60d5486ee23f1899bc8e389c07c3bc8c44ea
Rajahni fields <rajahnifields@gmail.com>
2021-01-14 00:38:41 -0500

    made readme file

--- Develop/index.js
+++ Develop/index.js
@@ -1,9 +1,15 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
-
+const util = require("util") //library
+const asyncWrite = util.promisify(fs.writeFile)
 // TODO: Create an array of questions for user input
-const questions = [
+
+const generateREADME = (answers) =>
+`<h1 class="display-4">Hi! My name is ${answers.name}</h1>`
+
+inquirer
+.prompt([
   {
     type: "input",
     name: "github",
@@ -46,28 +52,42 @@ const questions = [
   //   name: 'username',
   // },
   // {
-  //   type: 'input',
-  //   message: 'What is your user name?',
-  //   name: 'username',
-  // },
-];
-
+  //     type: 'input',
+  //     name: 'username',
+  //     message: 'What is your user name?',
+  //   },
+  ]).then((answers) => {
+          const filename = `${answers.github}.md`;
+          return asyncWrite(filename, generateREADME(answers))}
+      ) .then (()=>{ console.log("Success!")}).catch ((err)=>{console.log(err)});
+  
+      // err ? console.log(err) : console.log("Success!")
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-  fs.writeFile(fileName, data);
-// add something to filename to make it read the path of the current folder
-}
-
+// function writeFile(fileName, data) {
+//   fs.writeFile(fileName, data);
+//   const filename = `${generateREADME.github}.md`;
+//   return asyncWrite(fileName,data(questions))
+  
+    
+// // add something to filename to make it read the path of the current folder
+// }inquirer.prompt(questions).then((response) => {
+//   console.log(response);
+//   writeFile("README.md", GenMarkdown(questions))
+// })
 // TODO: Create a function to initialize app
-function init() {
-  inquirer.prompt(questions).then((response) => {
-    // console.log(response);
-    writeToFile("README.md", someFunctionToGenerateMarkdown())
-  });
-}
+// function init() {
+ 
+//   });
+// }
 
 // Function call to initialize app
-init();
+// init();
 
-// create the someFunctionToGenerateMarkdown whick should generate the right readme structure
+// create the someFunctionToGenerateMarkdown which should generate the right readme structure
 // pass into this function the response
+
+    // {
+    //     const filename = `${inquiryResponses.title}.md`;
+    //     return asyncWrite(filename, generateREADME(inquiryResponses) )}
+    // ) .then (()=>{ console.log("Success!")}).catch ((err)=>{console.log(err)}) ;
+    // err ? console.log(err) : console.log("Success!")
---
+++ Develop/th.md
@@ -0,0 +1,1 @@
+<h1 class="display-4">Hi! My name is undefined</h1>
